53a86e85f780b0066442fd5a44a7255ee101c236
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-24 10:07:52 +0530

    screen setup

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import All from './src/screens/All';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator> */}
+      <Tab.Navigator>
+        <Tab.Screen name="All" component={All} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
