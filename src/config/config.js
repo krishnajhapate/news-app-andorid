ae151c7a78b3da32cd87c922ba7a2c4f0c47855e
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-24 10:42:17 +0530

    news api setup

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+// generate you own api key from https://newsapi.org/
+export const API_KEY = `9493d9e8bc8c4fa8a232d9c67b37d025`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const country = `in`;
